338554aec5fb8fa6ae722a96ee3fd6bedfe68a4d
Faitza <faitzac@gmail.com>
2026-07-07 13:06:51 -0400

    Initialisation du projet

--- lib/main.dart
+++ lib/main.dart
@@ -1,59 +1,69 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:provider/provider.dart';
-import 'providers/auth_provider.dart';
-import 'screens/auth/role_selection_screen.dart';
-import 'screens/auth/auth_screen.dart';
-import 'screens/client/client_home_screen.dart';
-import 'screens/vendor/dashboard_screen.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(const CommercHaitiApp());
+void main() {
+  runApp(const MyApp());
 }
 
-class CommercHaitiApp extends StatelessWidget {
-  const CommercHaitiApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (_) => AuthProvider()..loadCurrentUser(),
-      child: MaterialApp(
-        title: 'CommercHaiti',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF003F88)),
-          useMaterial3: true,
-        ),
-        home: const AuthGate(),
-        routes: {
-          '/role': (_) => const RoleSelectionScreen(),
-          '/vendor': (_) => const VendorDashboardScreen(),
-        },
-        onGenerateRoute: (settings) {
-          if (settings.name == '/auth') {
-            final role = settings.arguments as String;
-            return MaterialPageRoute(
-              builder: (_) => AuthScreen(role: role),
-            );
-          }
-          return null;
-        },
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
       ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class AuthGate extends StatelessWidget {
-  const AuthGate({super.key});
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    final auth = context.watch<AuthProvider>();
-    if (auth.user == null) return const RoleSelectionScreen();
-    if (auth.user!.role == 'vendor') return const VendorDashboardScreen();
-    return const ClientHomeScreen();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
   }
 }
