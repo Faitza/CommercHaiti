b5a509884144edd8f4e407bec9fffdf80a634211
musted <mustedmf211@gmail.com>
2026-07-14 15:28:22 -0400

    Point d'entrée

--- lib/main.dart
+++ lib/main.dart
@@ -1,68 +1,72 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+// TODO : décommenter quand Firebase sera configuré
+// import 'package:firebase_core/firebase_core.dart';
+// import 'firebase_options.dart';
 
-void main() {
-  runApp(const MyApp());
+import 'constants/app_colors.dart';
+import 'providers/auth_provider.dart';
+import 'providers/cart_provider.dart';
+import 'providers/shop_provider.dart';
+import 'providers/order_provider.dart';
+import 'screens/auth/splash_screen.dart';
+// TODO : décommenter quand app_router.dart sera mergé
+// import 'router/app_router.dart';
+
+/// Point d'entrée — Falexson MERCIVAL
+/// Branch : feature/firebase-core (merge sur main après)
+/// Path : lib/main.dart
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // TODO Falexson : activer quand Firebase sera configuré
+  // await Firebase.initializeApp(
+  //   options: DefaultFirebaseOptions.currentPlatform,
+  // );
+
+  runApp(const CommercHaitiApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class CommercHaitiApp extends StatelessWidget {
+  const CommercHaitiApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => AuthProvider()),
+        ChangeNotifierProvider(create: (_) => CartProvider()),
+        ChangeNotifierProvider(create: (_) => ShopProvider()),
+        ChangeNotifierProvider(create: (_) => OrderProvider()),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'CommercHaiti',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(
+            seedColor: AppColors.navy,
+            primary: AppColors.navy,
+          ),
+          useMaterial3: true,
+          fontFamily: 'Roboto',
+          appBarTheme: const AppBarTheme(
+            backgroundColor: AppColors.navy,
+            foregroundColor: Colors.white,
+            elevation: 0,
+          ),
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: AppColors.navy,
+              foregroundColor: Colors.white,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(12),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        home: const SplashScreen(),
+        // TODO : remplacer par GoRouter quand app_router sera mergé
+        // routerConfig: AppRouter.router,
       ),
     );
   }
