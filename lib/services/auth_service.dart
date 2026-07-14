0db14c36f70cbcf1f683dd0bd4257413d49a4e24
musted <mustedmf211@gmail.com>
2026-07-14 15:06:41 -0400

    Service authentification Firebase

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,5 +1,5 @@
 // ignore_for_file: avoid_print
-// TODO Falexson : décommenter quand Firebase sera configuré
+// TODO Falexson Mercival : décommenter quand Firebase sera configuré
 // import 'package:firebase_auth/firebase_auth.dart';
 // import 'package:google_sign_in/google_sign_in.dart';
 // import 'package:cloud_firestore/cloud_firestore.dart';
